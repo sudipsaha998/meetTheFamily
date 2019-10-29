861223f9005a289aec9c311060823143c0f139f3
sudipsaha998 <sudipsaha998@gmail.com>
2019-10-30 01:13:47 +0530

    unit test cases for Person class methods

--- src/test/java/com/geektrust/problem/test/FamilyTreeTest.java
+++ src/test/java/com/geektrust/problem/test/FamilyTreeTest.java
@@ -10,11 +10,13 @@ import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import com.geektrust.problem.constants.FamilyConstants.RelationshipType;
 import com.geektrust.problem.exception.ChildAdditionException;
 import com.geektrust.problem.exception.PersonNotFoundException;
 import com.geektrust.problem.exception.RelationNotFoundException;
 import com.geektrust.problem.family.Family;
 import com.geektrust.problem.family.Person;
+import com.geektrust.problem.family.Relationship;
 import com.geektrust.problem.family.Relative;
 import com.geektrust.problem.service.FamilyService;
  
@@ -26,12 +28,51 @@ public class FamilyTreeTest {
  @BeforeClass 
  public static void init(){
 	 family = new Family(); 
+	 familyMembers = family.getFamilyMembers();
 	 }
 
+  @Test
+  public void checkGetPartnerForAPerson() {
+	  Person person = familyMembers.get("Vich");
+	  Person partner = new Person("Lika", "Female");
+	  Assert.assertEquals(Arrays.asList(new Person[] {partner}), person.getPartner());
+	  
+  }
+  
+  @Test
+  public void checkAddChildForAPerson() throws Exception {
+	  Person person = familyMembers.get("Lika");
+	  Person child = new Person("Vyks", "Male");
+	  person.addChild(child);
+	  Relationship childRelation = person.getRelationshipMap().get(RelationshipType.CHILD);
+	  Assert.assertTrue(childRelation.getRelatedPerson().contains(child));
+  }
+  
+  @Test
+  public void checkGetParentsForAPerson() {
+	  Person person = familyMembers.get("Vich");
+	  Person father = new Person("King Shan", "Male");
+	  Person mother = new Person("Queen Anga", "Female");
+	  Assert.assertEquals(Arrays.asList(new Person[] {father, mother}), person.getParents());
+	  
+  }
+  
+  @Test
+  public void checkGetChildrenForAPerson() {
+	  Person person = familyMembers.get("Dritha");
+	  Person child = new Person("Yodhan", "Male");
+	  Assert.assertEquals(Arrays.asList(new Person[] {child}), person.getChildren());
+	  
+  }
+  
+  @Test
+  public void testWhetherAPersonIsFemale() {
+	  Person person = new Person("Harry", "Male");
+	  Assert.assertFalse(Relative.checkIfFemale(person));
+  }
   
   @Test(expected = ChildAdditionException.class)
   public void childShouldNotGetAddedIfThePersonIsMale() throws Exception {  
-	  familyMembers = family.getFamilyMembers();
 	  Person person = family.findFamilyMember("Chit");
 		Person child = new Person("Srutak", "Male");
 		person.addChild(child);
@@ -76,7 +117,8 @@ public class FamilyTreeTest {
 	  String[] inputPhrase = "GET_RELATIONSHIP Dritha Brother-in-law".split(" ");
 	  FamilyService familyService = new FamilyService();
 	  Person person = family.findFamilyMember("Dritha");
-	  relatedPersons = Relative.getBrotherInLaw(person);
+	  Relative relative = new Relative();
+	  relatedPersons = relative.getBrotherInLaw(person);
 	  }
   
   
