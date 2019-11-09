a2dba947423c80a32ada4a8858b5cd440ca4faf8
sudipsaha998 <sudipsaha998@gmail.com>
2019-11-09 17:57:41 +0530

    added test suite

--- src/test/java/com/geektrust/problem/test/RelativeTest.java
+++ src/test/java/com/geektrust/problem/test/RelativeTest.java
@@ -20,7 +20,7 @@ import com.geektrust.problem.family.Relationship;
 import com.geektrust.problem.family.Relative;
 import com.geektrust.problem.service.FamilyService;
  
-public class FamilyTreeTest {
+public class RelativeTest {
  
 	private static Family family;
 	private static Relative relative;
@@ -32,40 +32,6 @@ public class FamilyTreeTest {
 	 relative = new Relative();
 	 familyMembers = family.getFamilyMembers();
 	 }
-
-  @Test
-  public void checkGetPartnerForAPerson() {
-	  Person person = familyMembers.get("Vich");
-	  Person partner = new Person("Lika", "Female");
-	  Assert.assertEquals(Arrays.asList(new Person[] {partner}), person.getPartner());
-	  
-  }
-  
-  @Test
-  public void checkAddChildForAPerson() throws Exception {
-	  Person person = familyMembers.get("Lika");
-	  Person child = new Person("Vyks", "Male");
-	  person.addChild(child);
-	  Relationship childRelation = person.getRelationshipMap().get(RelationshipType.CHILD);
-	  Assert.assertTrue(childRelation.getRelatedPerson().contains(child));
-  }
-  
-  @Test
-  public void checkGetParentsForAPerson() {
-	  Person person = familyMembers.get("Vich");
-	  Person father = new Person("King Shan", "Male");
-	  Person mother = new Person("Queen Anga", "Female");
-	  Assert.assertEquals(Arrays.asList(new Person[] {father, mother}), person.getParents());
-	  
-  }
-  
-  @Test
-  public void checkGetChildrenForAPerson() {
-	  Person person = familyMembers.get("Dritha");
-	  Person child = new Person("Yodhan", "Male");
-	  Assert.assertEquals(Arrays.asList(new Person[] {child}), person.getChildren());
-	  
-  }
   
   @Test
   public void testWhetherAPersonIsFemale() {
@@ -150,46 +116,6 @@ public class FamilyTreeTest {
 	  Person person = familyMembers.get("Krithi");
 	  Assert.assertEquals(Arrays.asList(new Person[] {new Person("Atya", "Female")}), relative.getPaternalAunt(person));
   }
-  
-  @Test(expected = ChildAdditionException.class)
-  public void childShouldNotGetAddedIfThePersonIsMale() throws Exception {  
-	  Person person = family.findFamilyMember("Chit");
-		Person child = new Person("Srutak", "Male");
-		person.addChild(child);
-		familyMembers.put("Srutak", child);
-	  }
-  
-  @Test
-  public void getDaughterOfAMother() {
-	  String[] inputPhrase = "GET_RELATIONSHIP Lika Daughter".split(" ");
-	  FamilyService familyService = new FamilyService();
-	  List<Person> expectedDaughterList = Arrays.asList(new Person[] {new Person("Vila", "Female"), new Person("Chika", "Female")});
-	  List<Person> daughterList = familyService.findRelatedPerson(inputPhrase, family);
-	  Assert.assertEquals(expectedDaughterList, daughterList);
-	  }
-  
-  @Test
-  public void getSonOfAFather() {
-	  String[] inputPhrase = "GET_RELATIONSHIP Vyan Son".split(" ");
-	  FamilyService familyService = new FamilyService();
-	  List<Person> expectedSonList = Arrays.asList(new Person[] {new Person("Asva", "Male"), new Person("Vyas", "Male")});
-	  List<Person> sonList = familyService.findRelatedPerson(inputPhrase, family);
-	  Assert.assertEquals(expectedSonList, sonList);
-	  }
-  
-  @Test
-  public void getBrotherInLawOfAPerson() {
-	  String[] inputPhrase = "GET_RELATIONSHIP Jaya Brother-in-law".split(" ");
-	  FamilyService familyService = new FamilyService();
-	  List<Person> expectedBrotherInLawList = Arrays.asList(new Person[] {new Person("Vritha", "Male")});
-	  List<Person> BrotherInLawList = familyService.findRelatedPerson(inputPhrase, family);
-	  Assert.assertEquals(expectedBrotherInLawList, BrotherInLawList);
-	  }
-  
-  @Test(expected = PersonNotFoundException.class)
-  public void expectExceptionIfPersonNotFound() throws Exception {
-	  family.findFamilyMember("Pjali");
-	  }
   
   @Test(expected = RelationNotFoundException.class)
   public void expectExceptionIfRelationNotFound() throws Exception {
